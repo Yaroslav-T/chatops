005997faad681ae680e6be5f24b410d148cd45ff
om <yaroslavmt@gmail.com>
2023-11-09 03:49:51 +0300

    init

---
+++ Makefile
@@ -0,0 +1,28 @@
+# Makefile
+
+# Variables
+VENV_NAME?=venv
+PYTHON=${VENV_NAME}/bin/python3
+ERRBOT=${VENV_NAME}//bin/errbot
+PIP=${VENV_NAME}/bin/pip3
+
+# Targets
+
+venv: $(VENV_NAME)/bin/activate
+
+$(VENV_NAME)/bin/activate: requirements.txt
+	test -d $(VENV_NAME) || virtualenv -p python3 $(VENV_NAME)
+	${PIP} install -Ur requirements.txt
+	touch $(VENV_NAME)/bin/activate
+
+docker:
+	docker run --name redis-errbot -d -p 6379:6379 -it redis/redis-stack:latest
+
+delete:
+	docker stop redis-errbot
+	docker rm redis-errbot
+
+errbot:
+	$(ERRBOT) -T
+
+start: venv docker errbot
---
+++ config.py
@@ -0,0 +1,31 @@
+import logging
+from pathlib import Path
+import os
+# This is a minimal configuration to get you started with the Text mode.
+# If you want to connect Errbot to chat services, checkout
+# the options in the more complete config-template.py from here:
+# https://raw.githubusercontent.com/errbotio/errbot/master/errbot/config-template.py
+
+current_directory = Path.cwd()
+
+BACKEND = "Text"  # Errbot will start in text mode (console only mode) and will answer commands from there.
+
+BOT_DATA_DIR = str(current_directory / r"data")
+BOT_EXTRA_PLUGIN_DIR = str(current_directory / r"plugins")
+BOT_EXTRA_BACKEND_DIR = str(current_directory / r"backend-plugins")
+
+BOT_LOG_FILE = str(current_directory / r"errbot.log")
+BOT_LOG_LEVEL = logging.INFO
+
+BOT_ADMINS = (
+    "@ADMIN",
+)
+
+BOT_EXTRA_STORAGE_PLUGINS_DIR = str(current_directory / r"data/plugins/errbotio/err-storage-redis")
+STORAGE = 'Redis'
+STORAGE_CONFIG = {
+    'host': 'localhost',
+    'port': 6379,
+    'db': 0,
+    'password': '',
+}
